df7fd991f88ddef09e840a2174f505855c574796
Eranda7896 <erandanadeeshan67@gmail.com>
2022-09-20 12:36:30 +0530

    added ui and model

---
+++ src/controller/StudentFormController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class StudentFormController {
+    public TableView tblStudent;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNic;
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtEmail;
+    public TextField txtContact;
+    public TextField txtNic;
+    public TextField txtAddress;
+
+    public void saveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void UpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void DeleteOnAction(ActionEvent actionEvent) {
+    }
+}
